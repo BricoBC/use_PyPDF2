2649898796e732ba135b53b7b5ad763b3bb1f58e
BricoBC <barajas.c.bruno.n@gmail.com>
2023-08-25 15:19:41 -0600

    start proyect and add first file

---
+++ README.md
@@ -0,0 +1,4 @@
+En èste repositorio se encuentra los primeros pasos para poder usar la libreria que nos permite hacer muchas funcionalidades con los pdf's.
+
+Para ejecutar en mi compu, utilizó el siguiente comando:
+python3 <archivo>.py
---
+++ metadata.py
@@ -0,0 +1,20 @@
+#Importamos la libreria con la cual trabajaremos
+import PyPDF2
+
+#Cargamos el archivo
+doc = PyPDF2.PdfReader("./documento.pdf")
+
+# Una variable tendrá todos los metadatos del documento
+meta = doc.metadata
+
+#Se imprime los valores de los metadato
+
+print(type(meta))
+print(meta)
+
+#Los metadatos están compuesto por:
+print(meta.author)
+print(meta.creator)
+print(meta.producer)
+print(meta.subject)
+print(meta.title)
