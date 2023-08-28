ffc2b2b5c9c6f2c77ea8c5ebb01e0720c21993d4
BricoBC <barajas.c.bruno.n@gmail.com>
2023-08-28 10:32:50 -0600

    add commennt in the file #3

--- merge.py
+++ merge.py
@@ -1,14 +1,20 @@
 import PyPDF2
 import os
 
+#Una variable que tiene propiedades para combinar los archivos
 merger = PyPDF2.PdfMerger()
 
+#Se crea una variable de tipo lista para almacenar los nombres de la ubicación que pasamos en el parametro
 files = os.listdir("./pdf's/")
-print(files)
 
+#Se hace el recorrido de cada archivo que existe en la lista
 for file in files:
+    #Se lee el archivo de una ubicación en especifico
     doc = PyPDF2.PdfReader("./pdf's/" + file)
+    #Se empieza a pilar los archivos.
     merger.append(doc)
 
+#Creamos el archivo, el parametro es el nombre del nuevo archivo.
 merger.write("merged.pdf")
+#Se cierra el archivo.
 merger.close()
