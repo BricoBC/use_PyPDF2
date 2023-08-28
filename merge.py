a8aefb8b25ddf31b0d36b79dc317cc03358ebbe1
BricoBC <barajas.c.bruno.n@gmail.com>
2023-08-28 11:11:23 -0600

    create 2 functions: merge for pages and merge to all

--- merge.py
+++ merge.py
@@ -1,20 +1,44 @@
 import PyPDF2
 import os
 
-#Una variable que tiene propiedades para combinar los archivos
-merger = PyPDF2.PdfMerger()
+def merge_all_files():
+    #Una variable que tiene propiedades para combinar los archivos
+    merger = PyPDF2.PdfMerger()
 
-#Se crea una variable de tipo lista para almacenar los nombres de la ubicación que pasamos en el parametro
-files = os.listdir("./pdf's/")
+    #Se crea una variable de tipo lista para almacenar los nombres de la ubicación que pasamos en el parametro
+    files = os.listdir("./pdf's/")
 
-#Se hace el recorrido de cada archivo que existe en la lista
-for file in files:
-    #Se lee el archivo de una ubicación en especifico
-    doc = PyPDF2.PdfReader("./pdf's/" + file)
-    #Se empieza a pilar los archivos.
-    merger.append(doc)
+    #Se hace el recorrido de cada archivo que existe en la lista
+    for file in files:
+        #Se lee el archivo de una ubicación en especifico
+        doc = PyPDF2.PdfReader("./pdf's/" + file)
+        #Se empieza a pilar los archivos.
+        merger.append(doc)
 
-#Creamos el archivo, el parametro es el nombre del nuevo archivo.
-merger.write("merged.pdf")
-#Se cierra el archivo.
-merger.close()
+    #Creamos el archivo, el parametro es el nombre del nuevo archivo.
+    merger.write("merged.pdf")
+    #Se cierra el archivo.
+    merger.close()
+
+def merge_for_pages():
+    merger = PyPDF2.PdfMerger()
+
+    files = os.listdir("./pdf's/")
+
+    for file in files:
+        doc = PyPDF2.PdfReader("./pdf's/" + file)
+        if (len(doc.pages)) != 1:
+            print('Para extraer un archivo se necesita una página de inicio y una página final')
+            pp1 = int(input(f"Para el archivo {file} cuál es la página de inicio: "))-1
+            pp2 = int(input(f"Para el archivo {file} cuál es la página final: "))-1
+            merger.append(fileobj= doc, pages= (pp1,pp2))
+            print()
+        else:
+            merger.append(doc)
+            
+
+    # Creamos el archivo, el parametro es el nombre del nuevo archivo.
+    merger.write("merged.pdf")
+    # Se cierra el archivo.
+    merger.close()
+merge_for_pages()
